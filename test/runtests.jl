d4dd8f73c8a658b1ecabb57c88d6f4a9bccc922a
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-31 15:22:29 +0800

    test: add tests for eltype(::ContinuedFraction)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using ContinuedFractions
 using Test
 
-include("OEIS.jl")
-import .OEIS
-include("cmp_oeis.jl")
+# include("OEIS.jl")
+# import .OEIS
+# include("cmp_oeis.jl")
+
+@testset "ContinuedFraction" begin
+    @testset "eltype" begin
+        # FiniteContinuedFraction{T<:Integer}
+        @test eltype(continuedfraction(1//42)) == Int
+        @test eltype(continuedfraction(big"1"//42)) == BigInt
+        # IrrationalContinuedFraction{T<:Integer, C}
+        @test eltype(continuedfraction(pi, Int32)) == Int32
+        @test eltype(continuedfraction(pi, BigInt)) == BigInt
+    end
+end
+
