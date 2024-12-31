e0585c8db90cc72f35214b4f7dc46a9a7f824fae
Documenter.jl <documenter@juliadocs.github.io>
2024-12-31 05:37:09 +0000

    build based on af5f048

---
+++ previews/PR3/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ContinuedFractions","category":"page"},{"location":"#ContinuedFractions","page":"Home","title":"ContinuedFractions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ContinuedFractions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ContinuedFractions]","category":"page"}]
+}
